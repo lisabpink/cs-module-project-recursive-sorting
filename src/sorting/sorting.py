35e28e584dd174099c0682b2553cca345e61f918
LISA_COOK <lisabpink@gmail.com>
2020-06-17 19:12:56 -0400

    sorting.py edits- tests passing

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -5,59 +5,67 @@ def merge(arrA, arrB):
 
     # Your code here
     # init pointers that start at each list
-    arrA_idx = 0
-    arrB_idx = 0
-    idx = 0
+    A_idx = 0
+    B_idx = 0
+    # idx = 0
 
     # traverse lists if starting index is less than length of array
-    while arrA_idx < len(arrA) and arrB_idx < len(arrB):
-        # if arrA_idx  is <  arrB_idx
-        if arrA[arrA_idx] < arrB[arrB_idx]:
-            # take index in merged array and set it to element of arrA_idx index
-            merged_arr[idx] = arrA[arrA_idx]
-            # increase merged_array by 1
-            idx += 1
-            # increase arrA_idx by 1
-            arrA_idx += 1
+    while A_idx < len(arrA) and B_idx < len(arrB):
 
-        # if arrA_idx is > arrB_idx
+        #! if A_idx  is <  B_idx
+        if arrA[A_idx] < arrB[B_idx]:
+            # take index in merged array and set it to element of A_idx index
+            merged_arr = arrA[A_idx]
+            # increase A_idx by 1
+            A_idx += 1
+
+        #! if A_idx is > B_idx
         else:
-            # take index in the merged array and set it to element of the arrB_idx index
-            merged_arr[idx] = arrB[arrB_idx]
-            # increase merged_array by 1
-            idx += 1
-            # increase arrB_idx by 1
-            arrB_idx += 1
+            # take index in the merged array and set it to element of the B_idxindex
+            merged_arr = arrB[B_idx]
+            # increase B_idxby 1
+            B_idx += 1
 
     #! traversing complete for one list
     # Add all of  elements from the other list to the combined one
 
-    # loop if starting index <  length of arrA_idx
-    while arrA_idx < len(arrA):
-        # take index in merged array and set it to element of arrA_idx index
-        merged_arr[idx] = arrA[arrA_idx]
-        # increase merged_array by 1
-        idx += 1
-        # increase arrA_idx by 1
-    while arrB_idx < len(arrB):
-        # take index in the merged array and set it to element of the arrB_idx index
-        merged_arr[idx] = arrB[arrB_idx]
-        # increase merged_array by 1
-        idx += 1
-        # increase arrB_idx by 1
-        arrB_idx += 1
-    print(f"merged array: {merged_arr}")
+    # loop if starting index <  length of A_idx
+    while A_idx < len(arrA):
+        # take index in merged array and set it to element of A_idx index
+        merged_arr = arrA[A_idx]
+        # increase A_idx by 1
+    while B_idx < len(arrB):
+        # take index in the merged array and set it to element of the B_idxindex
+        merged_arr = arrB[B_idx]
+        # increase B_idxby 1
+        B_idx += 1
+
+    # print(f"merged array: {merged_arr}")
     return merged_arr
+
+
+# A_idx = [1, 2, 3, 7]
+# B_idx = [0, 2, 5, 6, 8]
+# print(merge(A_idx, B_idx))
 
 # TO-DO: implement the Merge Sort function below recursively
 
 
 def merge_sort(arr):
     # Your code here
+
     if len(arr) > 1:
-        left = merge_sort(arr[:len(arr) // 2])
-        right = merge_sort(arr[len(arr) // 2:])
-        # merge the left and right arrays
+
+        # find the mid index so we can slice into left and right sublists
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        # recur on the left
+        left = merge_sort(left)
+        # recur on the right
+        right = merge_sort(right)
+
         arr = merge(left, right)
 
     return arr
